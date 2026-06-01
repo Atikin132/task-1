35ca635a8dac255a34fe9de6467fc38cbfdf8cb6
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-06-01 15:44:33 +0300

    refactor: refactor output with for

--- src/task2/Figure.java
+++ src/task2/Figure.java
@@ -12,4 +12,9 @@ public abstract class Figure implements Printable {
     public void print() {
         System.out.printf("This figure is a %s with a perimeter of %d\n", getName(), getPerimeter());
     }
+
+    public void printArea() {
+        System.out.println("Area of " + getName() + " is " + getArea());
+
+    }
 }
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -7,32 +7,28 @@ import task2.solids.Cone;
 import task2.solids.Cube;
 import task2.solids.Cylinder;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(2, 4, 3);
-        triangle.print();
-        System.out.println("Area of " + triangle.getName() + " is " + triangle.getArea());
 
+        ArrayList<Figure> figureArrayList = new ArrayList<>();
+        figureArrayList.add(new Triangle(2, 4, 3));
+        figureArrayList.add(new Square(5));
+        figureArrayList.add(new Trapeze(5, 10, 5, 4, 6));
 
-        Square square = new Square(5);
-        square.print();
-        System.out.println("Area of " + square.getName() + " is " + square.getArea());
+        for (Figure figure : figureArrayList) {
+            figure.print();
+            figure.printArea();
+        }
 
+        ArrayList<Solid> solidArrayList = new ArrayList<>();
+        solidArrayList.add(new Cube(5));
+        solidArrayList.add(new Cylinder(5, 10));
+        solidArrayList.add(new Cone(5, 10));
 
-        Trapeze trapeze = new Trapeze(5, 10, 5, 4, 6);
-        trapeze.print();
-        System.out.println("Area of " + trapeze.getName() + " is " + trapeze.getArea());
-
-
-        Cube cube = new Cube(5);
-        cube.print();
-
-
-        Cylinder cylinder = new Cylinder(5, 10);
-        cylinder.print();
-
-
-        Cone cone = new Cone(5, 10);
-        cone.print();
+        for (Solid solid : solidArrayList) {
+            solid.print();
+        }
     }
 }
