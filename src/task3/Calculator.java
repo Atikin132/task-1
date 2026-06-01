d969ee810bdf128497e1afd2f0fb54b0521a210e
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-06-01 17:21:31 +0300

    feat: add integer calculator

---
+++ src/task3/Calculator.java
@@ -0,0 +1,11 @@
+package task3;
+
+public interface Calculator<T> {
+    T sum(T a, T b);
+
+    T subtract(T a, T b);
+
+    T multiply(T a, T b);
+
+    T divide(T a, T b);
+}
---
+++ src/task3/calculator/IntegerCalculator.java
@@ -0,0 +1,25 @@
+package task3.calculator;
+
+import task3.Calculator;
+
+public class IntegerCalculator implements Calculator<Integer> {
+    @Override
+    public Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+
+    @Override
+    public Integer subtract(Integer a, Integer b) {
+        return a - b;
+    }
+
+    @Override
+    public Integer multiply(Integer a, Integer b) {
+        return a * b;
+    }
+
+    @Override
+    public Integer divide(Integer a, Integer b) {
+        return a / b;
+    }
+}
