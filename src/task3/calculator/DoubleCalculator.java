dc8e4bf1ea6e865b7e145890c606bae447518031
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-06-02 08:50:21 +0300

    feat: add double calculator

---
+++ src/task3/calculator/DoubleCalculator.java
@@ -0,0 +1,25 @@
+package task3.calculator;
+
+import task3.Calculator;
+
+public class DoubleCalculator implements Calculator<Double> {
+    @Override
+    public Double sum(Double a, Double b) {
+        return a + b;
+    }
+
+    @Override
+    public Double subtract(Double a, Double b) {
+        return a - b;
+    }
+
+    @Override
+    public Double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    @Override
+    public Double divide(Double a, Double b) {
+        return a / b;
+    }
+}
